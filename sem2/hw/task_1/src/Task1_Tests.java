2ee0f0dfea9c7180e927d25b1dcf41ac4b157a8b
ggsw4 <fdslpvmj@yandex.ru>
2022-03-27 23:48:11 +0300

    создан тест pushFront_pushToEmptyList_HeadAndTailIsCorrect

--- sem2/hw/task_1/src/Task1_Tests.java
+++ sem2/hw/task_1/src/Task1_Tests.java
@@ -7,4 +7,14 @@ public class Task1_Tests extends Assert {
         list.pushFront(10);
         assertEquals(1, list.getSize());
     }
+    @Test
+    public void pushFront_pushToEmptyList_HeadAndTailIsCorrect() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(10);
+        Node<Integer> head = (Node<Integer>) list.getHead();
+        Node<Integer> tail = (Node<Integer>) list.getTail();
+        assertEquals(10, head.getData().intValue());
+        assertEquals(10, tail.getData().intValue());
+        assertEquals(head, tail);
+    }
 }
