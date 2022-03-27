de94af7bf6f3ae04790f286a367db7c099fef2f4
ggsw4 <fdslpvmj@yandex.ru>
2022-03-28 00:47:29 +0300

    исправлена ошибка, добавлена в отчет

--- sem2/hw/task_1/src/DoubleLinkedList.java
+++ sem2/hw/task_1/src/DoubleLinkedList.java
@@ -8,7 +8,7 @@ public class DoubleLinkedList<T> {
     }
 
     public boolean isEmpty() {
-        return dummy == dummy;
+        return size == 0;
     }
     public int getSize() {
         return size;
