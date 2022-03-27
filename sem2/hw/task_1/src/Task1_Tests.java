cc17864315acb6c102c8dc93324f5fd89d8a78bb
ggsw4 <fdslpvmj@yandex.ru>
2022-03-27 23:46:19 +0300

    создан тест pushFront_pushToEmptyList_SizeEqualsOne

--- sem2/hw/task_1/src/Task1_Tests.java
+++ sem2/hw/task_1/src/Task1_Tests.java
@@ -1,4 +1,10 @@
 import org.junit.*;
 
 public class Task1_Tests extends Assert {
+    @Test
+    public void pushFront_pushToEmptyList_SizeEqualsOne () {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(10);
+        assertEquals(1, list.getSize());
+    }
 }
