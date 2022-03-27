6b1ef299637e04fc5d6eb0db333d68b441e4e01a
ggsw4 <fdslpvmj@yandex.ru>
2022-03-28 00:42:42 +0300

    добавлен тест createDoubleLinkedList_CreateEmptyDoubleLinkedList_EmptyFalse

--- sem2/hw/task_1/src/Task1_Tests.java
+++ sem2/hw/task_1/src/Task1_Tests.java
@@ -59,5 +59,10 @@ public class Task1_Tests extends Assert {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(list.isEmpty());
     }
-
+    @Test
+    public void createDoubleLinkedList_CreateEmptyDoubleLinkedList_EmptyFalse() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushBack(1);
+        assertFalse(list.isEmpty());
+    }
 }
