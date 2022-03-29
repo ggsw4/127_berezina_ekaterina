656836dde190f0f605f58d7f127e4307c44f19a6
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 05:40:26 +0300

    добавлен тест pushBack_PushBackNumbers_CheckValues

--- sem2/hw/task_1/src/Task1_Tests.java
+++ sem2/hw/task_1/src/Task1_Tests.java
@@ -368,4 +368,23 @@ public class Task1_Tests extends Assert {
         assertEquals(4, array.get(3).intValue());
         assertEquals(100, array.get(4).intValue());
     }
+
+    @Test
+    public void pushBack_PushBackNumbers_CheckValues(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        array.set(0, 1);
+        array.set(1, 2);
+        array.set(2, 3);
+        array.set(3, 4);
+        array.set(4, 5);
+        array.pushBack(6);
+        array.pushBack(7);
+        assertEquals(1, array.get(0).intValue());
+        assertEquals(2, array.get(1).intValue());
+        assertEquals(3, array.get(2).intValue());
+        assertEquals(4, array.get(3).intValue());
+        assertEquals(5, array.get(4).intValue());
+        assertEquals(6, array.get(5).intValue());
+        assertEquals(7, array.get(6).intValue());
+    }
 }
