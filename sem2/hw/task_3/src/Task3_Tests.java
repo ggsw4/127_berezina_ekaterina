23b8978bbf9aa91b36ce33a79728488c0a9345a7
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 20:25:16 +0300

    добавлен тест heapSort_CreateNewHeapSort_SortArray

--- sem2/hw/task_3/src/Task3_Tests.java
+++ sem2/hw/task_3/src/Task3_Tests.java
@@ -28,4 +28,17 @@ public class Task3_Tests extends Assert {
         sorter.sort(array, comparator);
         assertArrayEquals(new Integer[]{1, 2}, array);
     }
+    @Test
+    public void heapSort_CreateNewHeapSort_SortArray(){
+        HeapSort<Integer> sorter = new HeapSort<>();
+        Integer[] array = {1, 2, 7, 6, 9, 15, 13};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1, 2, 6, 7, 9, 13, 15}, array);
+    }
 }
