0bff8b094153f7700118faa329a4cf6f0e14a2ae
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 21:09:32 +0300

    добавлен тест mergeSort_CreateNewMergeSort_CompareTwoElementsArrays

--- sem2/hw/task_3/src/Task3_Tests.java
+++ sem2/hw/task_3/src/Task3_Tests.java
@@ -69,5 +69,18 @@ public class Task3_Tests extends Assert {
         sorter.sort(array, comparator);
         assertArrayEquals(new Integer[]{1}, array);
     }
+    @Test
+    public void mergeSort_CreateNewMergeSort_CompareTwoElementsArrays(){
+        MergeSort<Integer> sorter = new MergeSort<>();
+        Integer[] array = {1, 2};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1, 2}, array);
+    }
 
 }
