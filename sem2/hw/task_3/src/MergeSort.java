e4a7dfc9d8021d4b9deccaf9c689b20ee17e8aaa
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 21:34:22 +0300

    исправлена ошибка, добавлена в отчет

--- sem2/hw/task_3/src/MergeSort.java
+++ sem2/hw/task_3/src/MergeSort.java
@@ -14,7 +14,7 @@ public class MergeSort<T> implements Sort<T> {
         if(begin == end)
             return;
         int middle = (begin + end) / 2;
-        mergeSortRec(sorting, buffer, begin, middle);
+        mergeSortRec(buffer, sorting, begin, middle);
         mergeSortRec(sorting, buffer, middle + 1, end);
         merge(buffer, sorting, begin, middle - begin + 1, end - middle);
     }
