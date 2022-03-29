3a4129df9bb0fabe411817cfa2f26eb99a88bcf8
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 16:22:36 +0300

    hw

--- sem2/hw/task_2/src/Task2_Tests.java
+++ sem2/hw/task_2/src/Task2_Tests.java
@@ -1,4 +1,26 @@
 import org.junit.*;
 
 public class Task2_Tests extends Assert {
+    @Test
+    public void bubbleSort_CreateBubbleSort_TryToSort (){
+        BubbleSort<Integer> sorter = new BubbleSort<>();
+
+        MyComparator comparator = new MyComparator() {
+            @Override
+            public int compare(Object first, Object second) {
+                return 0;
+            }
+        };
+
+        Integer[] myArray = {1,2,3};
+        sorter.sort(myArray, comparator);
+        assertArrayEquals(new Integer[]{1, 2, 3}, myArray);
+    }
+
+    class SomeComparator implements MyComparator<Integer>{
+        @Override
+        public int compare(Integer first, Integer second) {
+            return first - second;
+        }
+    }
 }
