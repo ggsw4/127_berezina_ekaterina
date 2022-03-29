9cb1445bc92c47e3f1c2f85ea266a369883f6908
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 21:33:30 +0300

    добавлен тест mergeSort_CreateNewMergeSort_SortThreeElements

--- sem2/hw/task_3/src/Task3_Tests.java
+++ sem2/hw/task_3/src/Task3_Tests.java
@@ -108,4 +108,17 @@ public class Task3_Tests extends Assert {
         sorter.sort(array, comparator);
         assertArrayEquals(new Integer[]{1, 2}, array);
     }
+    @Test
+    public void mergeSort_CreateNewMergeSort_SortThreeElements(){
+        MergeSort<Integer> sorter = new MergeSort<>();
+        Integer[] array = {2, 1, 3};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1, 2, 3}, array);
+    }
 }
