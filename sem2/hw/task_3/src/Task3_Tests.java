19a4b28d93e95b84d4980b16b4d7ccebb5781c65
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 22:06:38 +0300

    добавлен тест quickSort_CreateNewQuickSort_CompareArrays

--- sem2/hw/task_3/src/Task3_Tests.java
+++ sem2/hw/task_3/src/Task3_Tests.java
@@ -148,4 +148,17 @@ public class Task3_Tests extends Assert {
         assertArrayEquals(new Integer[]{1, 2, 3, 5, 6, 8, 10, 12}, array);
     }
 
+    @Test
+    public void quickSort_CreateNewQuickSort_CompareArrays(){
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] array = {1,2};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1,2}, array);
+    }
 }
