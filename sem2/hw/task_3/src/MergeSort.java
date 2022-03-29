56963ff880b8190114a14d7a7d51def540c59933
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 21:42:11 +0300

    исправлена ошибка, добавлена в отчет

--- sem2/hw/task_3/src/MergeSort.java
+++ sem2/hw/task_3/src/MergeSort.java
@@ -15,7 +15,7 @@ public class MergeSort<T> implements Sort<T> {
             return;
         int middle = (begin + end) / 2;
         mergeSortRec(buffer, sorting, begin, middle);
-        mergeSortRec(sorting, buffer, middle + 1, end);
+        mergeSortRec(buffer, sorting, middle + 1, end);
         merge(buffer, sorting, begin, middle - begin + 1, end - middle);
     }
 
