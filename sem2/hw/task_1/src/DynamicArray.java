26eb80ae837af114f938cedbbce2f12548654e01
ggsw4 <fdslpvmj@yandex.ru>
2022-03-29 05:22:32 +0300

    исправлена ошибка, добавлена в отчет

--- sem2/hw/task_1/src/DynamicArray.java
+++ sem2/hw/task_1/src/DynamicArray.java
@@ -37,7 +37,7 @@ public class DynamicArray<T> {
         data[index] = value;
     }
     public void insert(int index, T value) {
-        if(index < 0 || index > size)
+        if(index < 0 || index >= size)
             throw new IndexOutOfBoundsException("Index out of bounds");
         if(capacity < size) {
             data = Arrays.copyOf(data, size * 2);
